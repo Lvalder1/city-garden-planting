7a87cacf88bf8101a1669e8d9bd981ae2b5dff19
Louis valder <louis_valder@hotmail.co.uk>
2020-09-22 22:01:45 +0100

    Added css to navbar links. More to be added

--- src/Components/navigation/navigation.jsx
+++ src/Components/navigation/navigation.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import "./navigation.module.scss";
-import 'bootstrap/dist/css/bootstrap.css' 
+import styles from "./navigation.module.scss";
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default class Navigation extends Component {
     render () {
         return (
             <Navbar bg="light" expand="sm" fixed="top">
-                <Navbar.Brand>City Garden Planting</Navbar.Brand>
+                <Navbar.Brand className={styles.brand}>
+                    <p id={styles.brandTop}>Carolyn Valder</p>
+                    <p id={styles.brandBottom}>City Garden Planting</p>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
+                    <Nav className="ml-auto mr-0">
                     <Nav.Link href="home">Home</Nav.Link>
                     <Nav.Link href="about">About</Nav.Link>
                     <Nav.Link href="service">The Service</Nav.Link>
