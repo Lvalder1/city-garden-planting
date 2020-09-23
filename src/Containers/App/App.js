668d1a0cf8f068a588649be6fe22f50b4992c3a7
Louis valder <louis_valder@hotmail.co.uk>
2020-09-23 12:18:05 +0100

    Added landing image on home page

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import styles from './App.module.scss';
 import Routes from '../../Routes/Routes';
 import Navigation from '../../Components/navigation';
-// import Contact from '../Contact';
 
 function App() {
   return (
       <div className={styles.App}>
         <Navigation />
         <Routes />
-        {/* <Contact /> */}
       </div>
   );
 }
--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import styles from "./Home.module.scss";
 import Contact from '../Contact';
+import img01 from "../../Images/img_01.png"
 
 
 export default class Home extends Component {
     render() {
         return (
             <div className={styles.home} id="home">
-                <h1>Home</h1>
+                <div className={styles.imgCenter}>
+                    <img src={img01} alt="img_01"></img>
+                </div>
                 <Contact />
             </div>
         )
