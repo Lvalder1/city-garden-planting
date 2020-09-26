ce076e7c934c8080d46b00c33604c6afe9f8b150
Louis valder <louis_valder@hotmail.co.uk>
2020-09-26 15:50:42 +0100

    Added photo gallery

--- src/Containers/Inspiration/Inspiration.jsx
+++ src/Containers/Inspiration/Inspiration.jsx
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
+import PhotoGallery from "../../Components/PhotoGallery";
 import styles from './Inspiration.module.scss';
+// import Img01 from "../../Images/Inspiration/img_01.jpg";
+// import Img02 from "../../Images/Inspiration/img_02.jpg";
+// import Img03 from "../../Images/Inspiration/img_03.jpg";
+// import Img04 from "../../Images/Inspiration/img_04.jpg";
+// import Img05 from "../../Images/Inspiration/img_05.jpg";
+// import Img06 from "../../Images/Inspiration/img_06.jpg";
+// import Img07 from "../../Images/Inspiration/img_07.jpg";
+// import Img08 from "../../Images/Inspiration/img_08.jpg";
+// import Img09 from "../../Images/Inspiration/img_09.jpg";
+// import Img10 from "../../Images/Inspiration/img_10.jpg";
+// import Img11 from "../../Images/Inspiration/img_11.jpg";
+// import Img12 from "../../Images/Inspiration/img_12.jpg";
+// import Img13 from "../../Images/Inspiration/img_13.jpg";
+// import Img14 from "../../Images/Inspiration/img_14.jpg";
+// import Img15 from "../../Images/Inspiration/img_15.jpg";
+// import Img16 from "../../Images/Inspiration/img_16.jpg";
+// import Img17 from "../../Images/Inspiration/img_17.jpg";
 
 export default class Inspiration extends Component {
-    render () {
+    render() {
         return (
-            <div className={styles.inspiration} id="inspiration">
-                <h1>Inspiration</h1>
-            </div>
+        <div className={styles.Inspiration}>
+            <PhotoGallery />
+        </div>
         )
     }
 }
