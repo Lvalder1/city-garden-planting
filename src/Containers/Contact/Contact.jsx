52e2f1eb1a4c7e3c27338900f258a7c1cef464ef
Louis valder <louis_valder@hotmail.co.uk>
2020-10-21 16:46:12 +0100

    Added contact page

--- src/Containers/Contact/Contact.jsx
+++ src/Containers/Contact/Contact.jsx
@@ -3,13 +3,26 @@ import styles from "./Contact.module.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelopeSquare} from '@fortawesome/free-solid-svg-icons';
 import { faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
+import { CV } from "../../Images/CV Louis Valder.pdf";
 
 export default class Contact extends Component {
     render () {
         return (
-            <div className={styles.contact} id="contact">
-                <a href="mailto:cwvalder@gmail.com"><FontAwesomeIcon icon={faEnvelopeSquare} size="2x" className={styles.fontawesome} /></a>
-                <FontAwesomeIcon icon={faInstagramSquare} size="2x" className={styles.fontawesome} />  
+            <div className={styles.contact} id="contactFooter">
+                <div className={styles.contactVia}>
+                    <p>If you would like to enquire about any of these services, do please email me at:</p>
+                </div>
+                <div className={styles.email}>
+                    <FontAwesomeIcon icon={faEnvelopeSquare} className={styles.fontawesome} /><a href="mailto:cwvalder@gmail.com" id={styles.contactEmail}>cwvalder@gmail.com</a>
+                </div>
+                <div className={styles.instagramFooter}>
+                    <div className={styles.instagram}>
+                        <FontAwesomeIcon icon={faInstagramSquare} className={styles.fontawesome} /><a href="https://www.instagram.com/carolynvalder/" id={styles.contactInstagram}>instagram.com/carolynvalder/</a> 
+                    </div>
+                    <div className={styles.createdBy}>
+                        <p>Website created by <a href={CV} target="_blank" rel="noopener noreferrer">Louis Valder</a></p>
+                    </div>
+                </div>
             </div>
         )
     }
