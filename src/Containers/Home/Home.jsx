85a85a60eb21f5718b88375824c99e30259316ee
Louis valder <louis_valder@hotmail.co.uk>
2020-10-24 15:49:25 +0100

    Added logo under main image

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styles from "./Home.module.scss";
 import ContactFooter from "../../Components/ContactFooter";
 import img01 from "../../Images/img_01.png";
+import websiteLogo from "../../Images/city-garden-planting-logo-new.png"
 
 export default class Home extends Component {
     render() {
@@ -11,7 +12,9 @@ export default class Home extends Component {
                 <div className={styles.imgCenter}>
                     <img src={img01} alt="img_01"></img>
                 </div>
-                <h1>City Garden Planting</h1>
+                <div className={styles.logo}>
+                    <img src={websiteLogo} alt="Website_Logo"></img>
+                </div>
             </div>
             <ContactFooter />
             </>
