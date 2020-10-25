059fca8c62fb1402501c40a98b54619e0d436233
Louis valder <louis_valder@hotmail.co.uk>
2020-10-25 18:01:14 +0000

    Added logo to contact page. More to follow

--- src/Containers/Contact/Contact.jsx
+++ src/Containers/Contact/Contact.jsx
@@ -4,25 +4,29 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelopeSquare} from '@fortawesome/free-solid-svg-icons';
 import { faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
 import { CV } from "../../Images/CV Louis Valder.pdf";
+import websiteLogo from "../../Images/city-garden-planting-logo-new.png"
 
 export default class Contact extends Component {
     render () {
         return (
-            <div className={styles.contact} id="contactFooter">
-                <div className={styles.contactVia}>
-                    <p>If you would like to enquire about any of these services, do please email me at:</p>
+            <div className={styles.contact} id="contact">
+                <div className={styles.centreDiv}>
+                    <img src={websiteLogo} alt="websiteLogo"></img>
+                    <div className={styles.contactVia}>
+                        <p>If you would like to enquire about any of these services, do please email me at:</p>
+                    </div>
+                    <div className={styles.email}>
+                        <FontAwesomeIcon icon={faEnvelopeSquare} className={styles.fontawesome} /><a href="mailto:cwvalder@gmail.com" id={styles.contactEmail}>cwvalder@gmail.com</a>
+                    </div>
                 </div>
-                <div className={styles.email}>
-                    <FontAwesomeIcon icon={faEnvelopeSquare} className={styles.fontawesome} /><a href="mailto:cwvalder@gmail.com" id={styles.contactEmail}>cwvalder@gmail.com</a>
-                </div>
-                <div className={styles.instagramFooter}>
+                {/* <div className={styles.instagramFooter}>
                     <div className={styles.instagram}>
                         <FontAwesomeIcon icon={faInstagramSquare} className={styles.fontawesome} /><a href="https://www.instagram.com/carolynvalder/" id={styles.contactInstagram}>instagram.com/carolynvalder/</a> 
                     </div>
                     <div className={styles.createdBy}>
                         <p>Website created by <a href={CV} target="_blank" rel="noopener noreferrer">Louis Valder</a></p>
                     </div>
-                </div>
+                </div> */}
             </div>
         )
     }
