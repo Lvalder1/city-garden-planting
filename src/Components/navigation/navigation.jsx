ea8f5128a3ede898ce9bf42d1294eff3af7a31fe
Louis Valder <louis.valder@nology.io>
2020-11-10 17:51:32 +0000

    Changed nav A link color

--- src/Components/navigation/navigation.jsx
+++ src/Components/navigation/navigation.jsx
@@ -4,12 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import styles from "./navigation.module.scss";
 import 'bootstrap/dist/css/bootstrap.css';
 import websiteLogo from '../../Images/city-garden-planting-logo-large.png';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "../../Containers/Home";
 import About from "../../Containers/About";
 import TheService from "../../Containers/TheService";
@@ -28,11 +23,11 @@ export default class Navigation extends Component {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto mr-0">
-                        <Link to="/">HOME</Link>
-                        <Link to="/about">ABOUT</Link>
-                        <Link to="/service">THE SERVICE</Link>
+                        <Link to="/" className={styles.navlink}>HOME</Link>
+                        <Link to="/about" className={styles.navlink}>ABOUT</Link>
+                        <Link to="/service" className={styles.navlink}>THE SERVICE</Link>
                         {/* <Nav.Link href="inspiration">Inspiration</Nav.Link> */}
-                        <Link to="/contact">CONTACT</Link>
+                        <Link to="/contact" v>CONTACT</Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
